157fada812a0931be2405cb7ba174ec7dbc4b61e
Chantel Yip <chantelyip422@gmail.com>
2020-05-28 00:51:35 +0000

    final push

--- hackbright.sql
+++ hackbright.sql
@@ -35,6 +35,42 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: grades; Type: TABLE; Schema: public; Owner: vagrant
+--
+
+CREATE TABLE public.grades (
+    id integer NOT NULL,
+    student_github character varying(30),
+    project_title character varying(30),
+    grade integer
+);
+
+
+ALTER TABLE public.grades OWNER TO vagrant;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE; Schema: public; Owner: vagrant
+--
+
+CREATE SEQUENCE public.grades_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.grades_id_seq OWNER TO vagrant;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: vagrant
+--
+
+ALTER SEQUENCE public.grades_id_seq OWNED BY public.grades.id;
+
+
+--
 -- Name: projects; Type: TABLE; Schema: public; Owner: vagrant
 --
 
@@ -107,6 +143,13 @@ ALTER SEQUENCE public.students_id_seq OWNED BY public.students.id;
 
 
 --
+-- Name: grades id; Type: DEFAULT; Schema: public; Owner: vagrant
+--
+
+ALTER TABLE ONLY public.grades ALTER COLUMN id SET DEFAULT nextval('public.grades_id_seq'::regclass);
+
+
+--
 -- Name: projects id; Type: DEFAULT; Schema: public; Owner: vagrant
 --
 
@@ -121,11 +164,23 @@ ALTER TABLE ONLY public.students ALTER COLUMN id SET DEFAULT nextval('public.stu
 
 
 --
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: vagrant
+--
+
+COPY public.grades (id, student_github, project_title, grade) FROM stdin;
+1	jhacks	Markov	10
+2	mary	johansen	30
+3	klean	kanteen	500
+\.
+
+
+--
 -- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: vagrant
 --
 
 COPY public.projects (id, title, description, max_grade) FROM stdin;
 1	Markov	Tweets generated from Markov chains	50
+2	poop	pooooop	100
 \.
 
 
@@ -140,10 +195,17 @@ COPY public.students (id, first_name, last_name, github) FROM stdin;
 
 
 --
+-- Name: grades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vagrant
+--
+
+SELECT pg_catalog.setval('public.grades_id_seq', 3, true);
+
+
+--
 -- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vagrant
 --
 
-SELECT pg_catalog.setval('public.projects_id_seq', 1, true);
+SELECT pg_catalog.setval('public.projects_id_seq', 2, true);
 
 
 --
@@ -151,6 +213,14 @@ SELECT pg_catalog.setval('public.projects_id_seq', 1, true);
 --
 
 SELECT pg_catalog.setval('public.students_id_seq', 2, true);
+
+
+--
+-- Name: grades grades_pkey; Type: CONSTRAINT; Schema: public; Owner: vagrant
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_pkey PRIMARY KEY (id);
 
 
 --
